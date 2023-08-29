fc0958bcdf7f9bcf16866ca170fef78d0bf0d28f
arnabdasrishi <arnabdasrishi@gmail.com>
2023-08-29 15:51:22 +0530

    reusable form created

--- Frontend/bb-app/src/Pages/auth/Login.js
+++ Frontend/bb-app/src/Pages/auth/Login.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
+import Form from "../../components/shared/Form/Form";
 
 const Login = () => {
   return (
     <>
-    <div className='row'>
-        <div className='col-md-8'>image</div>
-        <div className='col-md-4'>form</div>
-    </div>
-        
+      <div className="row g-0">
+        <div className="col-md-8 form-banner">
+          <img src="./assets/images/banner1.jpg" alt="login_image"></img>
+        </div>
+        <div className="col-md-4 form-container">
+          <Form formTitle={"Login Page"} submitButton={"Login"} />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- Frontend/bb-app/src/Pages/auth/Register.js
+++ Frontend/bb-app/src/Pages/auth/Register.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Form from '../../components/shared/Form/Form'
 
 const Register = () => {
   return (
-    <div>
-        <h1>Register Page</h1>   
-    </div>
+    <>
+     <div className='row g-0'>
+      <div className='col-md-8 form-banner'>
+        <img src='./assets/images/banner2.jpg' alt='register_image'/>
+      </div>
+      <div className='col-md-4 form-container'>
+        <Form  formTitle={"Register Page"} submitButton={"Register Now"} />
+      </div>
+     </div>
+    </>
   )
 }
 
