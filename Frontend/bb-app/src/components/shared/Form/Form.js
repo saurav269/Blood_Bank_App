412778fa691f71b265a66ca2ddbde17cfc849633
Saurv Mallik <sauravmallik786@.com>
2023-08-30 12:52:53 +0530

    fronted form & conditional rendering complete

--- Frontend/bb-app/src/components/shared/Form/Form.js
+++ Frontend/bb-app/src/components/shared/Form/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import InputType from "./InputType";
+import {Link} from 'react-router-dom'
 
 const Form = ({ formType, formTitle, submitButton }) => {
   const [email, setEmail] = useState("");
@@ -17,6 +18,61 @@ const Form = ({ formType, formTitle, submitButton }) => {
       <form>
         <h1 className="text-center">{formTitle}</h1>
         <hr />
+        <div className="d-flex mb-3">
+          <div className="form-check">
+            <input
+              type="radio"
+              className="form-check-input"
+              name="role"
+              id='donarRadio'
+              value={'donar'}
+              onChange={(e) => setRole(e.target.value)}
+              defaultChecked
+            />
+            <label htmlFor="donarRadio" className="form-check-label">
+              Donar
+            </label>
+          </div>
+          <div className="form-check ms-2">
+            <input
+              type="radio"
+              className="form-check-input"
+              name="role"
+              id='adminRadio'
+              value={'admin'}
+              onChange={(e) => setRole(e.target.value)}
+            />
+            <label htmlFor="adminRadio" className="form-check-label">
+              Admin
+            </label>
+          </div>
+          <div className="form-check ms-2">
+            <input
+              type="radio"
+              className="form-check-input"
+              name="role"
+              id='hospitalRadio'
+              value={'hospital'}
+              onChange={(e) => setRole(e.target.value)}
+            />
+            <label htmlFor="hospitalRadio" className="form-check-label">
+              Hospital
+            </label>
+          </div>
+          <div className="form-check ms-2">
+            <input
+              type="radio"
+              className="form-check-input"
+              name="role"
+              id='orgnisationRadio'
+              value={'organisation'}
+              onChange={(e) => setRole(e.target.value)}
+            />
+            <label htmlFor="orgnisationRadio" className="form-check-label">
+              Orgnisation
+            </label>
+          </div>
+        </div>
         {/* SWITCH CASE STATEMENT */}
         {(() => {
           //eslint-disable-next-line
@@ -47,6 +103,36 @@ const Form = ({ formType, formTitle, submitButton }) => {
             case formType === "register": {
               return (
                 <>
+                   {(role === 'admin' || role === 'donar') && (
+                    <InputType
+                    label={"Name"}
+                    labelFor={"forName"}
+                    type={"text"}
+                    name={"name"}
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  )}
+                  {role === 'organisation' && (
+                      <InputType
+                      label={"Organization Name"}
+                      labelFor={"forOrganizationName"}
+                      type={"text"}
+                      name={"organizationName"}
+                      value={organizationName}
+                      onChange={(e) => setOrganizationName(e.target.value)}
+                    />
+                  )}
+                  {role === 'hospital' && (
+                    <InputType
+                    label={"Hospital Name"}
+                    labelFor={"forHospitalName"}
+                    type={"text"}
+                    name={"hospitalName"}
+                    value={hospitalName}
+                    onChange={(e) => setHospitalName(e.target.value)}
+                  />
+                  )}
                   <InputType
                     label={"E-mail"}
                     labelFor={"forEmail"}
@@ -55,7 +141,6 @@ const Form = ({ formType, formTitle, submitButton }) => {
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                   />
-
                   <InputType
                     label={"Password"}
                     labelFor={"forPassword"}
@@ -64,34 +149,6 @@ const Form = ({ formType, formTitle, submitButton }) => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                   />
-
-                  <InputType
-                    label={"Name"}
-                    labelFor={"forName"}
-                    type={"text"}
-                    name={"name"}
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-
-                  <InputType
-                    label={"Organization Name"}
-                    labelFor={"forOrganizationName"}
-                    type={"text"}
-                    name={"organizationName"}
-                    value={organizationName}
-                    onChange={(e) => setOrganizationName(e.target.value)}
-                  />
-
-                  <InputType
-                    label={"Hospital Name"}
-                    labelFor={"forHospitalName"}
-                    type={"text"}
-                    name={"hospitalName"}
-                    value={hospitalName}
-                    onChange={(e) => setHospitalName(e.target.value)}
-                  />
-
                   <InputType
                     label={"Website"}
                     labelFor={"forWebsite"}
@@ -124,7 +181,15 @@ const Form = ({ formType, formTitle, submitButton }) => {
           }
         })()}
 
-        <div className="d-flex">
+        <div className="d-flex flex-row justify-content-between">
+          {formType === 'login' ? (
+            <p>Not Register Yet ? 
+              <Link to='/register'> Click Here !</Link>
+            </p>
+          ) : (<p>
+            Already User. Please!
+              <Link to='/login'> Login Here</Link>
+          </p>)}
           <button className="btn btn-primary" type="submit">
             {submitButton}
           </button>
--- Frontend/bb-app/src/components/shared/Form/InputType.js
+++ Frontend/bb-app/src/components/shared/Form/InputType.js
@@ -3,7 +3,7 @@ import React from "react";
 const InputType = ({value, onChange,name,type,label, labelFor}) => {
   return (
     <>
-      <div className="mb-3">
+      <div className="mb-1">
         <label htmlFor={labelFor} className="form-label">
           {label}
         </label>
