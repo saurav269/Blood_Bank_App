5ddc82389bbbb5ca80d1be8da8d6440f0a3e0769
Saurav Mallik <sauravmallik786@gmail.com>
2023-09-11 22:25:46 +0530

    Sidebar,Modal added with some style

--- Frontend/bb-app/src/Pages/HomePage.js
+++ Frontend/bb-app/src/Pages/HomePage.js
@@ -3,20 +3,33 @@ import { useSelector } from 'react-redux'
 import Spinner from '../components/shared/Form/Spinner'
 import { toast } from 'react-toastify'
 import Layout from '../components/shared/Layout/Layout'
+import Modal from './../components/shared/modal/Modal';
 
 const HomePage = () => {
   const {loading, error} = useSelector((state) => state.auth)
   return (
     <>
-    <Layout />
-    {error && <span>{toast.error(error)}</span>}
-    {loading ? (<Spinner />) : (
-      <>
-      <h1>Homepage</h1>
-      </>
-    )}  
+      <Layout>
+        {error && <span>{toast.error(error)}</span>}
+        {loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <h3
+              className="ms-4"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop"
+              style={{cursor : "pointer"}}
+            >
+              <i className="fa-solid fa-plus text-success py-4"></i>
+              Add Inventory
+            </h3>
+            <Modal />
+          </>
+        )}
+      </Layout>
     </>
-  )
+  );
 }
 
 export default HomePage
--- Frontend/bb-app/src/components/shared/Layout/Layout.js
+++ Frontend/bb-app/src/components/shared/Layout/Layout.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import Header from './Header'
+import Sidebar from './Sidebar'
 
-const Layout = ({childern}) => {
+
+
+const Layout = ({children}) => {
   return (
     <>
     <div className='header'>
         <Header />
     </div>
-    <div className='content'>{childern}</div>
-      
+    <div className='row g-1'>
+      <div className='col-md-3'>
+        <Sidebar/>
+      </div>
+      <div className='col-md-9'>{children}</div>
+    </div> 
     </>
   )
 }
