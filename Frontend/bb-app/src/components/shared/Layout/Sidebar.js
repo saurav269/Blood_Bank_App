735f02b5ed92ef6f01c5fe024ab622669e5e7d35
Saurav Mallik <sauravmallik786@gmail.com>
2023-09-18 00:23:40 +0530

    adding donar page functionality

--- Frontend/bb-app/src/components/shared/Layout/Sidebar.js
+++ Frontend/bb-app/src/components/shared/Layout/Sidebar.js
@@ -2,30 +2,52 @@ import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 // import { userMenu } from './Menu/userMenu'
 import '../../../Styles/Layout.css'
+import { useSelector } from 'react-redux';
 
 const Sidebar = () => {
     const location = useLocation()
+    const {user} = useSelector(state => state.auth)
     // const isActive = location.pathname
   return (
     <div>
       <div className="sidebar">
         <div className="menu">
-          <div className={`menu-item ${location.pathname === '/' && "active"}`}>
-            <i className="fa-solid fa-warehouse"></i>
-            <Link to="/">Inventory</Link>
-          </div>
-          <div className={`menu-item ${location.pathname === '/donar' && "active"}`}>
-            <i className="fa-solid fa-hand-holding-medical"></i>
-            <Link to="/donar">Donar</Link>
-          </div>
-          <div className={`menu-item ${location.pathname === '/hospital' && "active"}`}>
-            <i className="fa-solid fa-hospital"></i>
-            <Link to="/hospital">Hospital</Link>
-          </div>
-          <div className={`menu-item ${location.pathname === '/organisation' && "active"}`}>
-            <i className="fa-sharp fa-solid fa-building-ngo"></i>
-            <Link to="/organisation">Organisation</Link>
-          </div>
+          {user?.role === "organisation" && (
+            <>
+              <div
+                className={`menu-item ${location.pathname === "/" && "active"}`}
+              >
+                <i className="fa-solid fa-warehouse"></i>
+                <Link to="/">Inventory</Link>
+              </div>
+              <div
+                className={`menu-item ${
+                  location.pathname === "/donar" && "active"
+                }`}
+              >
+                <i className="fa-solid fa-hand-holding-medical"></i>
+                <Link to="/donar">Donar</Link>
+              </div>
+              <div
+                className={`menu-item ${
+                  location.pathname === "/hospital" && "active"
+                }`}
+              >
+                <i className="fa-solid fa-hospital"></i>
+                <Link to="/hospital">Hospital</Link>
+              </div>
+            </>
+          )}
+          {user?.role === "donar" && (
+            <div
+              className={`menu-item ${
+                location.pathname === "/organisation" && "active"
+              }`}
+            >
+              <i className="fa-sharp fa-solid fa-building-ngo"></i>
+              <Link to="/organisation">Organisation</Link>
+            </div>
+          )}
 
           {/* {userMenu.map((menu) =>{
                     const isActive = location.pathname === menu.path;
